5063ba2415614b69053903e446b3bd446984b4bd
Alexander Petrov <acepetrov96@gmail.com>
2020-09-05 16:06:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ function multiply(){
 function divide(){
 
 }
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
 function increment(n){
   return ++n;
 }
